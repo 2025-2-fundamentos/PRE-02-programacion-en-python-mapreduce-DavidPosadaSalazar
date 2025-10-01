b0431a907ac161c96d63b3b84e856b657caeb912
DavidPosadaSalazar <142468178+DavidPosadaSalazar@users.noreply.github.com>
2025-10-01 11:17:32 -0500

    commit2

--- homework/word_count.py
+++ homework/word_count.py
@@ -8,35 +8,94 @@ import string
 import time
 
 
-def initialize_directory(directory):
-    if os.path.exists(directory):
-        for file in glob.glob(f"{directory}/*"):
-            os.remove(file)
-    else:
-        os.makedirs(directory)
+def mapreduce(
+    mapper,
+    reducer,
+    input_dir,
+    output_dir,
+):
+    def read_lines_from_files(input_dir):
+        sequence = []
+        files = glob.glob(f"{input_dir}/*")
+        for file in files:
+            with open(file, "r", encoding="utf-8") as f:
+                for line in f:
+                    sequence.append((file, line))
+        return sequence
+
+    def apply_shuffle_and_sort(pairs_sequence):
+        pairs_sequence = sorted(pairs_sequence)
+        return pairs_sequence
+
+    def write_results_to_file(result, output_dir):
+        with open(f"{output_dir}/part-00000", "w", encoding="utf-8") as f:
+            for key, value in result:
+                f.write(f"{key}\t{value}\n")
+
+    def create_success_file(output_dir):
+        with open(f"{output_dir}/_SUCCESS", "w", encoding="utf-8") as f:
+            f.write("")
+
+    def create_output_dir_or_fail(output_dir):
+        if os.path.exists(output_dir):
+            raise Exception(f"Output directory '{output_dir}' already exists.")
+        else:
+            os.makedirs(output_dir)
+
+    sequence = read_lines_from_files(input_dir)
+    pairs_sequence = mapper(sequence)
+    pairs_sequence = apply_shuffle_and_sort(pairs_sequence)
+    result = reducer(pairs_sequence)
+    create_output_dir_or_fail(output_dir)
+    write_results_to_file(result, output_dir)
+    create_success_file(output_dir)
 
 
-def copy_and_number_raw_files_to_input_folder(raw_dir, input_dir, n=5000):
-    for file in glob.glob(f"{raw_dir}/*"):
-        with open(file, "r", encoding="utf-8") as f:
-            text = f.read()
+def run_experiment(
+    n,
+    mapper,
+    reducer,
+    raw_dir,
+    input_dir,
+    output_dir,
+):
 
-        for i in range(1, n + 1):
-            raw_filename_with_extension = os.path.basename(file)
-            raw_filename_without_extension = os.path.splitext(
-                raw_filename_with_extension
-            )[0]
-            new_filename = f"{raw_filename_without_extension}_{i}.txt"
-            with open(f"{input_dir}/{new_filename}", "w", encoding="utf-8") as f2:
-                f2.write(text)
-def read_lines_from_files(input_dir):
-    sequence = []
-    files = glob.glob(f"{input_dir}/*")
-    for file in files:
-        with open(file, "r", encoding="utf-8") as f:
-            for line in f:
-                sequence.append((file, line))
-    return sequence
+    def initialize_directory(directory):
+        if os.path.exists(directory):
+            for file in glob.glob(f"{directory}/*"):
+                os.remove(file)
+        else:
+            os.makedirs(directory)
+
+    def copy_and_number_raw_files_to_input_folder(raw_dir, input_dir, n=5000):
+        for file in glob.glob(f"{raw_dir}/*"):
+            with open(file, "r", encoding="utf-8") as f:
+                text = f.read()
+
+            for i in range(1, n + 1):
+                raw_filename_with_extension = os.path.basename(file)
+                raw_filename_without_extension = os.path.splitext(
+                    raw_filename_with_extension
+                )[0]
+                new_filename = f"{raw_filename_without_extension}_{i}.txt"
+                with open(f"{input_dir}/{new_filename}", "w", encoding="utf-8") as f2:
+                    f2.write(text)
+
+    initialize_directory(input_dir)
+    copy_and_number_raw_files_to_input_folder(raw_dir, input_dir, n)
+
+    start_time = time.time()
+
+    mapreduce(
+        mapper,
+        reducer,
+        input_dir,
+        output_dir,
+    )
+
+    end_time = time.time()
+
+    print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
 
 
 def wordcount_mapper(sequence):
@@ -51,10 +110,6 @@ def wordcount_mapper(sequence):
     return pairs_sequence
 
 
-# Ordena la secuencia de pares por la palabra. Este es el shuffle and sort.
-def apply_shuffle_and_sort(pairs_sequence):
-    pairs_sequence = sorted(pairs_sequence)
-    return pairs_sequence
 def wordcount_reducer(pairs_sequence):
     result = []
     for key, value in pairs_sequence:
@@ -65,31 +120,17 @@ def wordcount_reducer(pairs_sequence):
     return result
 
 
-def write_results_to_file(result, output_dir):
-    with open(f"{output_dir}/part-00000", "w", encoding="utf-8") as f:
-        for key, value in result:
-            f.write(f"{key}\t{value}\n")
+if __name__ == "__main__":
+    
+    run_experiment(
+        n=5,  # or any number you want
+        mapper=wordcount_mapper,
+        reducer=wordcount_reducer,
+        raw_dir="files/raw",
+        input_dir="files/input",
+        output_dir="files/output",
+    )
 
+    #
 
-def create_success_file(output_dir):
-    with open(f"{output_dir}/_SUCCESS", "w", encoding="utf-8") as f:
-        f.write("")
-
-
-initialize_directory("files/input")
-copy_and_number_raw_files_to_input_folder("files/raw", "files/input", 100)
-
-start_time = time.time()
-
-sequence = read_lines_from_files("files/input")
-pairs_sequence = wordcount_mapper(sequence)
-pairs_sequence = apply_shuffle_and_sort(pairs_sequence)
-result = wordcount_reducer(pairs_sequence)
-initialize_directory("files/output/")
-write_results_to_file(result, "files/output")
-
-create_success_file("files/output")
-
-end_time = time.time()
-
-print(f"Tiempo de ejecuciÃ³n: {end_time - start_time:.2f} segundos")
+    
